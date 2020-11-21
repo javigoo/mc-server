c440e1d04849482fdf87feeb8f6067a254a894bf
javigoo <javierroiggregorio@gmail.com>
2020-11-21 15:40:50 +0100

    setup.sh

--- scripts/setup.sh
+++ scripts/setup.sh
@@ -1,10 +1,5 @@
 #!/bin/bash
-
-# Variables de configuración
-server_path='../server/'
-server_files_path='../server_files/'
-server_memory_configuration='-Xmx1024M -Xms1024M'
-sleep_threshold='3'
+source server.config
 
 # Variables globales
 server_name=$(ls $server_files_path | grep '.jar')
@@ -17,31 +12,36 @@ cd $server_path
     mv $server_name 'server.jar'
 
     # Inicializar servidor
-    echo 'First server start'
+    echo 'Initializing first server start'
     java $server_memory_configuration -jar server.jar nogui >/dev/null
 
     echo 'Accepting EULA'
     echo 'eula=true' > eula.txt
 
     echo 'Starting server to generate configuration files'
-    screen -d -m -L -S mcs java $server_memory_configuration -jar server.jar nogui 
+    screen -L -dmS mcs java $server_memory_configuration -jar server.jar nogui 
 
-    # Finalizar servidor
-    while [ true ]
+    # Espera hasta que se inicia por completo el servidor
+    while [ -z $(cat screenlog.0 | grep -o 'Done') ]
     do
-        IsServerActive=$(cat screenlog.0 | grep -o 'Done')
-        if [ ! -z $IsServerActive ]
+        #echo $(cat screenlog.0 | grep -o 'Done')
+        #echo $(tail -n 1 screenlog.0)
+        ServerException=$(cat screenlog.0 | grep -o 'Exception stopping the server')
+        if [ ! -z "$ServerException" ]
         then
-            echo "Shutting down server"
-            #screen -r mcs -X stuff 'stop'
-            #screen -r mcs -X stuff "$(printf \\r)"
-            break 
+            echo "Server exception: Stopping the server" >&2
+            exit 1
         fi
+        
         sleep $sleep_threshold
     done
 
+    # Finalizar servidor
+    screen -r mcs
+    screen -S mcs -X stuff 'stop'
+    screen -S mcs -X stuff "$(printf \\r)"
+
     # Actualizar archivos personalizados
-    echo "Updating customized files"
+    echo "Updating customized configuration files"
     cp -r $server_files_path $server_path
     rm $server_name
-
